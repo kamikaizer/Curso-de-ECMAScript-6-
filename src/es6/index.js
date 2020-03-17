63ec57676fcab0432ffb6d2f64568dee5d4329d9
Martín Matayoshi <kamikaizer@gmail.com>
2020-03-17 18:03:07 -0300

    concatenar

--- src/es6/index.js
+++ src/es6/index.js
@@ -12,3 +12,16 @@ function newFunction2(name = 'oscar', age = 32, country ="MX"){
 
 newFunction2();
 newFunction2('ricardo', '23', 'CO');
+
+// Como concatenar
+
+// Modo largo
+
+// let hello = "Hello";
+// let world = "world";
+// let epicPhrase = hello + ' ' + world;
+// console.log(epicPhrase);
+
+// Modo cort
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
